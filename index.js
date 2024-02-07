052bc245ffe280fd5bd040703dfd34404a88d4d0
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-02-07 19:40:48 +0200

    add code to create qr code png

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 1. Use the inquirer npm package to get user input.*/
 
 import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([{ message: "Type in your URL: ", name: "URL" }])
   .then((answers) => {
     const url = answers.URL;
+    let qr_png = qr.image(url);
+    qr_png.pipe(fs.createWriteStream("qr_img.png"));
   })
   .catch((error) => {
     if (error.isTtyError) {
